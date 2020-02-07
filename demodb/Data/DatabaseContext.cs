55e559e3256f5e67d44e0a65a63bc448ef988c61
David Karlsson <david.karlsson1@learnet.se>
2020-02-07 11:25:15 +0100

    Add project files.

---
+++ demodb/Data/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using demodb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace demodb.Data
+{
+    public class DatabaseContext : DbContext
+    {
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<demodb.Models.Customer> Customer { get; set; }
+    }
+    
+    }
---
+++ demodb/Migrations/20200204110710_New mig.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace demodb.Migrations
+{
+    public partial class Newmig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customer");
+        }
+    }
+}
